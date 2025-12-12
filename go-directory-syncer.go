c1d5d47ee615b2a0680c3527f3b19b7512a75193
volodymyr <volodymyr.oliinyk.name@gmail.com>
2025-12-11 20:23:33 -0500

    ubuntu service

--- go-directory-syncer.go
+++ go-directory-syncer.go
@@ -1,5 +1,6 @@
 package main
 
+// TODO:[1]: https://gemini.google.com/app/023edbcc85a65189, sudo nano /etc/systemd/system/go-directory-syncer.service
 import (
     "flag"
     "fmt"
---
+++ sh/go-directory-syncer-wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+INSTANCE_NAME=$1
+
+# --- DEFINE PATH DEFINITION LOGIC HERE ---
+# The simplest option: using the case operator to specify paths by instance name
+case $INSTANCE_NAME in
+    projectA)
+        SOURCE="/home/$USER/data/source_A"
+        DESTINATION="/mnt/backup/dest_A"
+        ;;
+    projectB)
+        SOURCE="/home/$USER/docs/source_B"
+        DESTINATION="/mnt/cloud/dest_B"
+        ;;
+    # Add other sync pairs here
+    *)
+        echo "Error: Unknown syncer instance name: $INSTANCE_NAME" >&2
+        exit 1
+        ;;
+esac
+
+# Running your Go script with defined paths
+exec /usr/local/bin/go-directory-syncer \
+    --source-directory="$SOURCE" \
+    --destination-directory="$DESTINATION"
